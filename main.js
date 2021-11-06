373b0d06a5fdd30c943222ce8a47f65c148e71a5
VenyaNuthakki <71914805+VenyaNuthakki@users.noreply.github.com>
2021-11-06 09:44:52 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -3,10 +3,24 @@ function preload(){
 }
 
 function draw(){
-
+    background("#baabff");
 }
 
 function setup(){
     canvas=createCanvas(500,500);
-    canvas.center();
+    canvas.position(560,100);
+    video=createCapture(VIDEO);
+    video.size(550,500);
+    posenet=ml5.poseNet(video,modelloaded);
+    posenet.on('pose',getposes)
+}
+
+function modelloaded(){
+    console.log("modelloaded");
+}
+
+function getposes(results){
+    if (results.length>0){
+        console.log(results);
+    }
 }
