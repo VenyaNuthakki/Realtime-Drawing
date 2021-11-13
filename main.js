6d1370e45d005c1a25df6f00da021587705fc341
VenyaNuthakki <71914805+VenyaNuthakki@users.noreply.github.com>
2021-11-13 09:50:51 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
+nosex=0;
+nosey=0;
+leftwristx=0;
+rightwristx=0;
+difference=0;
 function preload(){
 
-}
-
-function draw(){
-    background("#baabff");
 }
 
 function setup(){
@@ -22,5 +23,17 @@ function modelloaded(){
 function getposes(results){
     if (results.length>0){
         console.log(results);
+        nosex=results[0].pose.nose.x;
+        nosey=results[0].pose.nose.y;
+        leftwristx=results[0].pose.leftWrist.x;
+        rightwristx=results[0].pose.rightWrist.x;
+        difference=floor(leftwristx-rightwristx);
     }
+}
+
+function draw(){
+    background("#baabff");
+    fill("#0acaff");
+    square(nosex,nosey,difference);
+    document.getElementById("square_side").innerHTML="size of the square : "+difference+" px";
 }
